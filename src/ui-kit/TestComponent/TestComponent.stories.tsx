ee79ae365144e2273a2cf9cec78a4412790d81a3
totes8 <86833007+totes8@users.noreply.github.com>
2024-08-14 14:02:51 +0200

    commit test

---
+++ src/ui-kit/TestComponent/TestComponent.stories.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import TestComponent from './TestComponent';
+
+export default {
+  title: "TestComponent",
+};
+
+export const Default = () => <TestComponent />;
+
+Default.story = {
+  name: 'default',
+};
