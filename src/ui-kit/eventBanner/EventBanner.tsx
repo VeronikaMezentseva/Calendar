b7b29a960e05086bb4e5eb3d7d3b79d1866e1b5a
totes8 <86833007+totes8@users.noreply.github.com>
2024-08-14 22:30:26 +0200

    updated eventBanner

--- src/ui-kit/eventBanner/EventBanner.tsx
+++ src/ui-kit/eventBanner/EventBanner.tsx
@@ -1,14 +1,14 @@
-import React, { FC } from 'react';
-import style from './style.module.css'
-import AccessibleForwardIcon from '@mui/icons-material/AccessibleForward';
-
-
+import React, { FC } from "react";
+import style from "./style.module.css";
+import AccessibleForwardIcon from "@mui/icons-material/AccessibleForward";
 
 export const EventBanner: FC = () => {
-   return(
-     <div className={style.container}>
-      <div className={style["title-container"]}><p className={style["event-title"]}>Workout</p><AccessibleForwardIcon className={style.icon}/></div>
+  return (
+    <div className={style.container}>
+      <div className={style["title-container"]}>
+        <p className={style["event-title"]}>Workout</p><AccessibleForwardIcon style={{ fontSize: '11px' }} />
+      </div>
       <p className={style["event-time"]}>16:00-18:00</p>
-     </div>
-   );
- }
+    </div>
+  );
+};
